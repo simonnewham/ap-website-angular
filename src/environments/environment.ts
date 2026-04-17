6dc411401a75287b0061a18e3ce82a274a98b9ed
Simon Newham <snewham09@gmail.com>
2026-04-17 09:31:24 +0200

    c

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   firebaseApi: 'https://ap-website-a4dfd.firebaseio.com',
-  apiKey: 'AIzaSyCSjlXD5HRcQiVEwLf3OSbXVjUio43YE0o',
+  apiKey: '',
 };
